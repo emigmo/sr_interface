2e7da54e8965df2ba24af4fd7643b4daf881829c
Toni Oliver <toni@shadowrobot.com>
2015-10-28 16:49:21 +0000

    Add pick place example

---
+++ sr_example/scripts/sr_example/hand_and_arm_examples/sr_pick_place.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+# Example to demonstrate moving to stored/names targets. Both arm and hand movements executed.
+# Available named targets can be viewed in MoveIt, on the planning tab.
+
+import rospy
+from sr_robot_commander.sr_arm_commander import SrArmCommander
+from sr_robot_commander.sr_hand_commander import SrHandCommander
+from sr_utilities.hand_finder import HandFinder
+
+rospy.init_node("pick_place", anonymous=True)
+
+hand_finder = HandFinder()
+
+hand_parameters = hand_finder.get_hand_parameters()
+hand_serial = hand_parameters.mapping.keys()[0]
+
+hand_commander = SrHandCommander(hand_parameters=hand_parameters,
+                                 hand_serial=hand_serial)
+arm_commander = SrArmCommander()
+
+
+def wait_for_topic():
+    try:
+        input("Press enter to continue")
+    except:
+        pass
+
+while not rospy.is_shutdown():
+    wait_for_topic()
+    arm_commander.move_to_named_target("arm_pre_bracket", False)
+    hand_commander.move_to_named_target("hand_pre_bracket", False)
+    wait_for_topic()
+    arm_commander.move_to_named_target("arm_grasp_bracket", False)
+    wait_for_topic()
+    hand_commander.move_to_named_target("hand_grasp_bracket", True)
+    wait_for_topic()
+    arm_commander.move_to_named_target("arm_pre_bracket", False)
+    wait_for_topic()
+    arm_commander.move_to_named_target("arm_pre_place_bracket", False)
+    wait_for_topic()
+    arm_commander.move_to_named_target("arm_place_bracket", False)
+    wait_for_topic()
+    hand_commander.move_to_named_target("hand_pre_bracket", False)
+    wait_for_topic()
+    arm_commander.move_to_named_target("arm_pre_place_bracket", False)
+    wait_for_topic()
+    arm_commander.move_to_named_target("arm_place_bracket", False)
+    wait_for_topic()
+    hand_commander.move_to_named_target("hand_grasp_bracket", True)
+    wait_for_topic()
+    arm_commander.move_to_named_target("arm_pre_place_bracket", False)
+    wait_for_topic()
+    arm_commander.move_to_named_target("arm_pre_bracket", False)
+    wait_for_topic()
+    arm_commander.move_to_named_target("arm_grasp_bracket", False)
+    wait_for_topic()
+    hand_commander.move_to_named_target("hand_pre_bracket", False)
